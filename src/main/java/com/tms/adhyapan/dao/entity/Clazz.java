1302780487dfc872331145323d5ac34bd07cea76
Talukder <Kaushik.Talukder@Staples.com>
2018-12-09 00:01:30 -0500

    Add class entity

--- src/main/java/com/tms/adhyapan/dao/entity/Clazz.java
+++ src/main/java/com/tms/adhyapan/dao/entity/Clazz.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import org.hibernate.annotations.Formula;
 
@@ -14,24 +15,23 @@ import lombok.Data;
 
 @Entity
 @Data
-public class Teacher implements Serializable {
+@Table(name = "class")
+public class Clazz implements Serializable {
+ 
+	private static final long serialVersionUID = -1558838374123302898L;
 
-	private static final long serialVersionUID = 4569152174501395948L;
-	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String firstName;
-	private String lastName;
-	private String teacherCode;
-	private String contact;
-	private String address;
-	private Long schoolId;
-	@Formula(value = "(select s.school_name from school s where s.id = school_id)")
-	private String schoolName;
+	
 	private Long subjectId;
 	@Formula(value = "(select s.subject_name from subject s where s.id = subject_id)")
 	private String subjectName;
+	
+	private Long teacherId;
+	private Long standardId;
+	private Long centerId;
+	
 	private Timestamp createdAt = new Timestamp(System.currentTimeMillis());
 	private Timestamp updatedAt = new Timestamp(System.currentTimeMillis());
 	private Timestamp endDate;

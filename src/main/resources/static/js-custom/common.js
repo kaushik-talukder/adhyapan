9d127005b7d50e12f3dd418dd49d4eec5c04068b
Kaushik Talukder <kaushik.talukder@staples.com>
2018-12-10 14:51:23 -0500

    Create class form

--- src/main/resources/static/js-custom/common.js
+++ src/main/resources/static/js-custom/common.js
@@ -21,3 +21,30 @@ function populateSubjectDropdown(){
 		$('#select-subject-dropdown').selectpicker('refresh');
 	});
 }
+
+function populateStandardDropdown(){
+	$("#select-standard-dropdown").load('populateAllStandards', {
+		pageFragment : 'components/data-component.html :: frag-standard-dropdown'
+	}, function(response, status, xhr) {
+		thymeleafFragmentResponseCheck(response);
+		$('#select-standard-dropdown').selectpicker('refresh');
+	});
+}
+
+function populateCenterDropdown(){
+	$("#select-center-dropdown").load('populateAllCenters', {
+		pageFragment : 'components/data-component.html :: frag-center-dropdown'
+	}, function(response, status, xhr) {
+		thymeleafFragmentResponseCheck(response);
+		$('#select-center-dropdown').selectpicker('refresh');
+	});
+}
+
+function populateTeacherDropdown(){
+	$("#select-teacher-dropdown").load('populateAllTeachers', {
+		pageFragment : 'components/data-component.html :: frag-teacher-dropdown'
+	}, function(response, status, xhr) {
+		thymeleafFragmentResponseCheck(response);
+		$('#select-teacher-dropdown').selectpicker('refresh');
+	});
+}

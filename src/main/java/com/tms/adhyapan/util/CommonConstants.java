10fa2342757279726461a5ed7993cd22b3bd8039
Kaushik Talukder <kaushik.talukder@staples.com>
2018-12-13 15:34:09 -0500

    fee collection

--- src/main/java/com/tms/adhyapan/util/CommonConstants.java
+++ src/main/java/com/tms/adhyapan/util/CommonConstants.java
@@ -3,6 +3,7 @@ package com.tms.adhyapan.util;
 public class CommonConstants {
 	public static final String ACTIVE_YES = "Y";
 	public static final String ACTIVE_NO = "N";
+	
 	public static final String GENDER_MALE = "M";
 	public static final String GENDER_FEMALE = "F";
 	
@@ -15,4 +16,8 @@ public class CommonConstants {
 	public static final String END_DATE_DEFAULT = "3000-12-31";
 	public static final String DATE_FORMAT_DEFAULT = "yyyy-MM-dd";
 	public static final String DATE_FORMAT_YYYYMMM = "yyyy-MMM";
+	
+	public static final int FEE_CAT_REGISTRATION = 1;
+	public static final int FEE_CAT_TUTION = 2;
+	public static final int FEE_CAT_EXAM = 3;
 }
--- src/main/resources/static/js-custom/collect-tution-fee.js
+++ src/main/resources/static/js-custom/collect-tution-fee.js
@@ -13,9 +13,31 @@ $(document).ready(function() {
 	
 	$('body').on('click', '.class-option', function() {
 		var classId = $("#select-class-dropdown").val();
-		populateClassFee(classId);
-		populateClassMonthDropdown(classId);
 		populateStudentDropdown(null,classId);
+	});
+	
+	$('body').on('click', '.student-option', function() {
+		var classId = $("#select-class-dropdown").val();
+		populateClassMonthDropdown(classId);
+		populateClassFee(classId);
+	});
+	$('body').on('click', '.class-month-option', function() {
+		var classId = $("#select-class-dropdown").val();
+		populateClassFee(classId);
+	});
+	$('body').on('click', '#btn-save-tution-fee', function() {
+		$("#tution-fee-confirm-message").dialog({
+			modal : true,
+			buttons : {
+				Save : function() {
+					$(this).dialog("close");
+					saveTutionFee();
+				},
+				Cancel : function(){
+					$(this).dialog("close");
+				}
+			}
+		});
 	});
 });
 
@@ -28,6 +50,61 @@ function populateClassFee(classId){
 		},
 		dataType : "json"
 	}).done(function(response) {
-		$("#fee-amount").val(response);
+		$("#total-fee-amount").val(response);
 	});
+}
+
+function saveTutionFee(){
+	var classId = $("#select-class-dropdown").val();
+	var studentId = $("#select-student-dropdown").val();
+	var monthCode = $("#select-class-month-dropdown").val();
+	var feeCategoryId= $("#fee-category-id").val();
+	var remarks = $("#remarks").val();
+	var feeAmount = 0;
+	if(monthCode.length > 0){
+		feeAmount = $("#total-fee-amount").val() / monthCode.length;
+	} else {
+		feeAmount = $("#total-fee-amount").val();
+	}
+	
+	var studentTutionFee = [];
+	$(monthCode).each(function(index, value){
+		studentTutionFee.push({
+			classId: classId,
+			studentId: studentId,
+			monthCode: value,
+			feeAmount: feeAmount,
+			feeCategoryId : feeCategoryId,
+			remarks:remarks
+		});
+	});
+	
+	$.ajax({
+		method : "POST",
+		url : "saveTutionFee",
+		data : JSON.stringify(studentTutionFee),
+		dataType : "json",
+		contentType: "application/json"
+	}).done(function(response) {
+		resetForm();
+		$("#tution-fee-dialog-message").dialog({
+			modal : true,
+			buttons : {
+				Ok : function() {
+					$(this).dialog("close");
+				}
+			}
+		});
+	});
+	
+}
+
+function resetForm(){
+	$("#select-subject-dropdown").val('').selectpicker('refresh');
+	$("#select-teacher-dropdown").val('').selectpicker('refresh');
+	$("#select-class-dropdown").val('').selectpicker('refresh');
+	$("#select-student-dropdown").val('').selectpicker('refresh');
+	$("#select-class-month-dropdown").val('').selectpicker('refresh');
+	$("#total-fee-amount").val("");
+	$("#remarks").val("");
 }
